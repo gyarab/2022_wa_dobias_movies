e33ae4338935fc94f3de513a4d8d1171c4593cba
Your Name <you@example.com>
2023-03-21 09:54:36 +0100

    funguj!

--- gamdb/films/admin.py
+++ gamdb/films/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 from .models import Movie
-# Register your models here.
 
-admin.site.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'year']
+    list_display_links = ['name']
+    search_fields = ['name', 'description']
+
+admin.site.register(Movie, MovieAdmin)
