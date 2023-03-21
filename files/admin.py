4daf93043ae708c17f325af099dfa02a3fae0f73
Your Name <you@example.com>
2023-03-21 08:10:00 +0100

    doplneny models.py a admin.py

--- files/admin.py
+++ files/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Movie
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'year']
+    list_display_links = ['name']
+    search_fields = ['name', 'description']
+
+admin.site.register(Movie, MovieAdmin)
