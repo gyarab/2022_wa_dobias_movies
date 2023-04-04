612d83eeb0d3677cbc8ba82b73cf368f8b6d380e
Your Name <you@example.com>
2023-04-04 11:22:26 +0200

    xxx

--- gamdb/films/views.py
+++ gamdb/films/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-from .models import Movie
+from .models import Movie, Director
+
 
 def homepage(request):
     context = {
-        'movies' : Movie.objects.all()
+        'movies': Movie.objects.all()
     }
     return render(request, 'main.html', context)
-    #return HttpResponse("hee hee")
+    # return HttpResponse("hee hee")
+
+
+def directors(request):
+    context = {
+        'title': 'Reziseri',
+        'directors': Director.objects.all(),
+    }
+    return render(request, 'directors.html', context)
