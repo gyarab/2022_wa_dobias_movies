5faa4f8e1e508cdc39631312eb362621d0dc3830
Your Name <you@example.com>
2023-05-17 10:12:16 +0200

    hmmmmmm

--- gamdb/films/forms.py
+++ gamdb/films/forms.py
@@ -1,18 +1,19 @@
 from django import forms
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class CommentForm(forms.Form):
     author = forms.CharField(
         required=False,
-        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Pepa z Depa"})
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Pepík's pipík owner"})
     )
     text = forms.CharField(
         required=False,
-        widget=forms.Textarea(attrs={"rows": 5, "class": "form-control"})
+        widget=forms.Textarea(attrs={"rows": 5, "class": "form-control", "placeholder": "Coment about Pepík's pipík"})
     )
     rating = forms.IntegerField(
         required=False,
-        widget=forms.NumberInput(attrs={"class": "form-control"}),
+        widget=forms.NumberInput(attrs={"class": "form-control", "placeholder": "Pepík's pipík rating"}),
         validators=[
             MaxValueValidator(100),
             MinValueValidator(1)
